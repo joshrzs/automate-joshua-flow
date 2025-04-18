9216355d6e1494851958df18b907d65cf54fe8dd
Josh Balando <joshbalando@gmail.com>
2025-04-19 00:42:15 +0800

    NavSttt

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Menu, X } from 'lucide-react';
 import ThemeToggle from './ThemeToggle';
 import "../styles/glow.css";
-
+import "../styles/navbar.css";
 
 
 const Navbar = () => {
